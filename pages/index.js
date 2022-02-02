58101705a50a6c4d5378c26b4057969613e1dabe
gnjnshh <31447786+gnjnshh@users.noreply.github.com>
2022-02-02 07:09:31 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app, Emjoy!" />
+        <Header title="Welcome to my app, Emjoy and come again!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
